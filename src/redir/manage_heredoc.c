b76e7662e6c628b43408234278b817db2131a359
Amina EL KHATTABI <ael-khat@e2r9p8.clusters.42paris.fr>
2022-05-27 16:50:13 +0200

    quand un heredoc : OK

--- src/redir/manage_heredoc.c
+++ src/redir/manage_heredoc.c
@@ -1,4 +1,5 @@
 #include "../../includes/minishell.h"
+#include "../../gnl/get_next_line.h"
 #include "../../libft/libft.h"
 
 int	manage_one_redir(t_datas *delimiter)
@@ -6,29 +7,23 @@ int	manage_one_redir(t_datas *delimiter)
 	t_datas	*copy;
 	int		file;
 	char	*buf;
+	char	*test;
+
 
 	copy = delimiter;
 	file = open("file", O_TRUNC | O_CREAT | O_RDWR, 0000644);
 	while (1)
     {
-        write(1, ">", 2);
+        write(1, "> ", 3);
 		buf = get_next_line(0);
+		//printf("length copy->data : %ld --- length : buf == %ld\n", ft_strlen(copy->data), ft_strlen(buf));
         if (buf == NULL)
             exit(1);
-		while(copy)
-		{
-
-			if (copy->type == 33 || copy->type == 34)
-			{
-				copy = copy->next;
-				break ;
-			}
-			copy = copy->next;
-		}
-        if (!ft_strncmp(copy->data, buf, ft_strlen(copy->data) + 1))
+		//printf("comparaison %d \n", ft_strncmp(copy->data, buf, ft_strlen(copy->data)));
+        if (!ft_strncmp(copy->data, buf, ft_strlen(copy->data)))
             break ;
         write(file, buf, ft_strlen(buf));
-        write(file, "\n", 1);
+        write(file, "\n", 2);
         free(buf);
 	}
 }
