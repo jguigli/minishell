2069653c072e08b3920e015c696e2d67ed0ed5ff
Amina EL KHATTABI <ael-khat@e2r9p8.clusters.42paris.fr>
2022-06-10 14:42:07 +0200

    Quelques tests OKi

--- minishell.c
+++ minishell.c
@@ -8,9 +8,15 @@ int	main(int argc, char *argv[], char *env[])
 	(void)argv;
 	manage_signal();
 	init_global();
-	if (!search_in_env_var("PATH", env))
-		return (-1); // MSG ERROR 
+	// if (!search_in_env_var("PATH", env))
+	// {
+	// 	printf("looooool\n");
+	// 	return (-1); // MSG ERROR 
+	// }
 	g.env = get_copy(env); //ATTENTION PLUSIEURS MALLOC DANS GET_COPY. A FREE A LA FIN
+	int i = 0;
+	// while (g.env[i])
+	// 	printf("env = %s\n", g.env[i++]);
 	get_prompt(g.env);
 	return (0);
 }
